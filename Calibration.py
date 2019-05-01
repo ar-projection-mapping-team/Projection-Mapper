4a016402cb421436eab03f25dd6b761b1b42ae4b
Fedja Milz <hubswarm@gmail.com>
2019-04-30 18:06:52 -0700

    Added ability to change Canny threshold on the fly.

--- Calibration.py
+++ Calibration.py
@@ -9,34 +9,41 @@ class OutlineCalibration:
         # INIT IMAGES #
         #   Save original image, edge-detected greyscale image, and edge-detected color image:
         self.image = input_image
-        self.edge_image = cv2.Canny(input_image, 100, 240)
-        self.edge_color = cv2.cvtColor(self.edge_image, cv2.COLOR_GRAY2RGB)
         #   Save width and height of image:
         self.width = w
         self.height = h
+        #   Call function to create the edge detected images, with the default thresholds.
+        self.create_edge_image(240)
+
+    # Updates the threshold for the Canny edge detector and re-defines the image variables
+    def create_edge_image(self, thresh):
+
+        # UPDATE IMAGES #
+        self.edge_image = cv2.Canny(self.image, 100, thresh)
+        self.edge_color = cv2.cvtColor(self.edge_image, cv2.COLOR_GRAY2RGB)
 
         # CREATE EDGE PIXEL LOCATION LIST #
         #   Create list that holds locations of pixels that belong to an edge:
         self.edges = []
         for i in range(self.height):
             for j in range(self.width):
-                if self.edge_image[i,j] == 255:
-                    self.edges.append((i,j))
+                if self.edge_image[i, j] == 255:
+                    self.edges.append((i, j))
         #   Create list that holds locations of pixels bordering edges:
         self.border_edges = []
         for x in range(len(self.edges)):
-            (i,j) = self.edges[x]
+            (i, j) = self.edges[x]
             if j != 0:
-                n = (i,j-1)
+                n = (i, j - 1)
                 self.border_edges.append(n)
             if i != self.height - 1:
-                e = (i+1,j)
+                e = (i + 1, j)
                 self.border_edges.append(e)
             if j != self.width - 1:
-                s = (i,j+1)
+                s = (i, j + 1)
                 self.border_edges.append(s)
             if i != 0:
-                w = (i-1,j)
+                w = (i - 1, j)
                 self.border_edges.append(w)
 
     # Applies shader to a single frame of the object edge calibration output
