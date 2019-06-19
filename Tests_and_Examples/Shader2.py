440d83ad3e72f79f97902fbdc153ef3241ce7daf
Fedja Milz <hubswarm@gmail.com>
2019-06-18 20:28:24 -0700

    Created initial version of new shader method (Shader2.py).

---
+++ Tests_and_Examples/Shader2.py
@@ -0,0 +1,68 @@
+import cv2
+
+class Shader:
+
+    def __init__(self, source_path):
+
+        # Get source image, as well as its height and width
+        self.source = cv2.imread(source_path, 0)
+        self.source_height, self.source_width = self.source.shape[:2]
+
+        # Define shader image components:
+        # shader_image refers to the entire image created by the shader, while shader_pixels refers to only the pixels
+        # that belong to an edge of the Canny filter
+        self.shader_image = []
+        self.shader_pixels = []
+
+    # Creates a new shader with specified threshold, brightness and contrast values
+    # TODO: ADD FUNCTIONALITY FOR BRIGHTNESS AND CONTRAST
+    def create_shader(self, threshold, brightness, contrast):
+
+        # Create list of pixels (using HSV color space) for the shader
+        canny_filter = cv2.Canny(self.source, 100, threshold)
+        x = cv2.cvtColor(canny_filter, cv2.COLOR_GRAY2RGB)
+        self.shader_image = cv2.cvtColor(x, cv2.COLOR_RGB2HSV)
+
+        # Create list that holds locations of pixels that belong to an edge
+        for i in range(self.source_height):
+            for j in range(self.source_width):
+                if self.shader_image[i, j][2] != 0:
+                    self.shader_pixels.append((i, j))
+
+    # Generates a single frame of the shader
+    def generate_frame(self, iterator, mod):
+
+        # Copy the shaded image
+        frame = self.shader_image
+
+        # For each pixel actually belonging to an edge (each visible pixel only) apply the shader's effect over time
+        # (this is controlled by the 'iterator' argument).
+        for i in range(self.shader_pixels.__len__()):
+            current_pixel_location= self.shader_pixels[i]
+            frame[current_pixel_location] = (iterator, 100, 100)
+
+        return frame
+
+    # Shows the shader window
+    # TODO: IMPLEMENT MODIFIER FUNCTIONALITY
+    def show_shader(self):
+
+        # Define the iterator and modifier values
+        i = 0
+        m = 0
+
+        # Display frame after frame of the shader onto output window
+        while True:
+
+            # Create the current frame by calling the frame generator function with the iterator and modifier arguments
+            current_frame = self.generate_frame(i, m)
+
+            # Show the current frame, use waitKey to show it only for a small amount of time
+            cv2.imshow('Shader', current_frame)
+            cv2.waitKey(1)
+
+            # If the HSV hue channel reaches 359, loop it back around to 0
+
+            print(i)
+            # Iterate
+            i = i + 1
---
+++ Tests_and_Examples/Shader2Driver.py
@@ -0,0 +1,6 @@
+from Tests_and_Examples.Shader2 import Shader
+
+image_path = "bird.jpg"
+shader = Shader(image_path)
+shader.create_shader(500, 0, 0)
+shader.show_shader()
